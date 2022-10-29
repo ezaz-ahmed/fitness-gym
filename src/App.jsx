b830327ea1d5e30a8764dc0aac4a65c803d80f56
Ezaz <ezaz.dev@gmail.com>
2022-10-29 22:26:54 +0600

    🔨: Banner changege, ð👕ð: Styles added

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,10 @@ import Footer from './components/Footer'
 
 const App = () => {
   return (
-    <Box width="400px" xs={{ width: { xl: '1488px' } }} m="auto">
+    <Box
+      width="400px"
+      xs={{ width: { xl: '1488px' } }}
+      m="auto">
 
       <Navbar />
 
@@ -16,7 +19,6 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/exercise/:id" element={<ExerciseDetail />} />
       </Routes>
-
       <Footer />
     </Box>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import { useState } from 'react';
+import { Box } from '@mui/material';
+import HeroBanner from '../components/HeroBanner';
 
 const Home = () => {
+  const [exercises, setExercises] = useState([]);
+  const [bodyPart, setBodyPart] = useState('all');
+
   return (
-    <div>Home</div>
+    <Box>
+      <HeroBanner />
+      {/* <HeroBanner />
+      <SearchExercises setExercises={setExercises} bodyPart={bodyPart} setBodyPart={setBodyPart} />
+      <Exercises setExercises={setExercises} exercises={exercises} bodyPart={bodyPart} /> */}
+    </Box>
   )
 }
 
